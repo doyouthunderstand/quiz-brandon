8131aa7232349ca4aa464bdd04e516e542b09145
Brandon Gorman <doyouthunderstand@gmail.com>
2016-03-29 21:06:12 +0000

    Set up database, migration, model and controller

---
+++ db/migrate/20160329210316_create_quizzes.rb
@@ -0,0 +1,11 @@
+class CreateQuizzes < ActiveRecord::Migration
+  def change
+    create_table :quizzes do |t|
+      
+      t.string :name
+      t.text :description
+
+      t.timestamps 
+    end
+  end
+end
